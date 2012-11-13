d9ebeae9c53b7f4bcec2bf6a45f930163c105a53
Johannes Würbach <johannes.wuerbach@googlemail.com>
2012-11-13 23:42:24 +0100

    Added android-tester

---
+++ src/de/softgames/tester/CommonUtilities.java
@@ -0,0 +1,23 @@
+package de.softgames.tester;
+
+/**
+ * Helper class providing methods and constants common to other classes in the
+ * app.
+ */
+public final class CommonUtilities {
+
+	/**
+	 * URL of the notification server
+	 */
+	static final String SERVER_URL = "http://mobile-notifications.softgames.de/devices";
+
+	/**
+	 * Google API project id registered to use GCM.
+	 */
+	static final String SENDER_ID = "99374070246";
+
+	/**
+	 * Tag used on log messages.
+	 */
+	static final String STAG = "de.softgames.tester";
+}
