a5b3af9e997b4e7a49f99783a065260c875f266a
Roland Castillo <rolandaugusto@gmail.com>
2013-02-14 13:00:04 +0100

    Readability improved

--- src/de/softgames/tester/CommonUtilities.java
+++ src/de/softgames/tester/CommonUtilities.java
@@ -1,21 +1,18 @@
 package de.softgames.tester;
 
+
 /**
  * Helper class providing methods and constants common to other classes in the
  * app.
  */
 public final class CommonUtilities {
 
-	/**
-	 * URL of the notification server
-	 */
-	static final String SERVER_URL = "http://mobile-notifications.softgames.de/devices";
-	//static final String SERVER_URL = "http://192.168.0.13:3000/devices";	
-	//static final String SERVER_URL = "http://10.0.2.2:3000/devices";
+    /* URL of the notification server */
+    static final String SERVER_URL = "http://mobile-notifications.softgames.de/devices";
+    // static final String SERVER_URL = "http://192.168.0.13:3000/devices";
+    // static final String SERVER_URL = "http://10.0.2.2:3000/devices";
 
-	/**
-	 * Google API project id registered to use GCM.
-	 */
-	static final String SENDER_ID = "1079642456342";
+    /* Google API project id registered to use GCM. */
+    static final String SENDER_ID = "1079642456342";
 
 }
--- src/de/softgames/tester/MainActivity.java
+++ src/de/softgames/tester/MainActivity.java
@@ -1,5 +1,6 @@
 package de.softgames.tester;
 
+
 import static de.softgames.tester.CommonUtilities.SENDER_ID;
 import android.app.Activity;
 import android.content.Context;
@@ -15,111 +16,114 @@ import android.widget.TextView;
 
 import com.google.android.gcm.GCMRegistrar;
 
+
 public class MainActivity extends Activity {
 
-	private static final String TAG = "MainActivity";
-	private AsyncTask<Void, Void, Void> _registerTask;
-	private TelephonyManager telephonyManager;
-	/**
-	 * MNC: Mobile network code
-	 * MCC: Mobile communication component 
-	 */
-	private TextView provider, mnc, mcc;
+    private static final String TAG = "MainActivity";
+    private AsyncTask<Void, Void, Void> _registerTask;
+    private TelephonyManager telephonyManager;
+    /*
+     * MNC: Mobile network code MCC: Mobile communication component
+     */
+    private TextView provider, mnc, mcc;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		final Resources res = getResources();
-		provider = (TextView) findViewById(R.id.provider_name_value);
-		mcc = (TextView) findViewById(R.id.provider_mcc_value);
-		mnc = (TextView) findViewById(R.id.provider_mnc_value);
-		
-		try {
-			Log.d(TAG, "Trying to get mobile data connection info...");
-			// Provider test
-			telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-			String simOperator = telephonyManager.getSimOperatorName();
-			if (simOperator != null && !simOperator.equals("")) {
-				Log.d(TAG, "provider name: " + simOperator);
-				provider.setText(simOperator);
-				mcc.setText(simOperator.substring(0, 3));
-				mnc.setText(simOperator.substring(3));
-			} else {
-				Log.d(TAG, "No mobile data found, get wifi info instead");			
-				WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-				WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-				//Service set identifier for a 802.11 network
-				String wifiSSID = String.format(res.getString(R.string.wifi_connection), wifiInfo.getSSID());
-				provider.setText(wifiSSID);
-				mcc.setText(res.getString(R.string.not_applicable));
-				mnc.setText(res.getString(R.string.not_applicable));	
-			}
-		} catch (Exception e) {
-			Log.e(TAG, "Unknown error", e);		
-		}		
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Resources res = getResources();
+        provider = (TextView) findViewById(R.id.provider_name_value);
+        mcc = (TextView) findViewById(R.id.provider_mcc_value);
+        mnc = (TextView) findViewById(R.id.provider_mnc_value);
 
-		GCMRegistrar.checkDevice(this);
-		GCMRegistrar.checkManifest(this);
-		final String regId = GCMRegistrar.getRegistrationId(this);
-		
-		if (regId == null || regId.equals("")) {	
-			Log.d(TAG, "regId is empty");
-			GCMRegistrar.register(getApplicationContext(), SENDER_ID);
-		} else {
-			Log.d(TAG, "registered with regId: " + regId);
-			GCMRegistrar.setRegisteredOnServer(this, false);
-			if (!GCMRegistrar.isRegisteredOnServer(this)) {
-				final Context context = getApplicationContext();
-				_registerTask = new AsyncTask<Void, Void, Void>() {
+        try {
+            Log.d(TAG, "Trying to get mobile data connection info...");
+            // Provider test
+            telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            String simOperator = telephonyManager.getSimOperatorName();
+            if (simOperator != null && !simOperator.equals("")) {
+                Log.d(TAG, "provider name: " + simOperator);
+                provider.setText(simOperator);
+                mcc.setText(simOperator.substring(0, 3));
+                mnc.setText(simOperator.substring(3));
+            } else {
+                Log.d(TAG, "No mobile data found, get wifi info instead");
+                WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                // Service set identifier for a 802.11 network
+                String wifiSSID = String.format(
+                        res.getString(R.string.wifi_connection),
+                        wifiInfo.getSSID());
+                provider.setText(wifiSSID);
+                mcc.setText(res.getString(R.string.not_applicable));
+                mnc.setText(res.getString(R.string.not_applicable));
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Unknown error", e);
+        }
 
-					@Override
-					protected Void doInBackground(Void... params) {
-						boolean registered = ServerUtilities.register(context,
-								regId);
-						// At this point all attempts to register with the app
-						// server failed, so we need to unregister the device
-						// from GCM - the app will try to register again when
-						// it is restarted. Note that GCM will send an
-						// unregistered callback upon completion, but
-						// GCMIntentService.onUnregistered() will ignore it.
-						if (!registered) {
-							GCMRegistrar.unregister(context);
-						}
-						
-						return null;
-					}
+        GCMRegistrar.checkDevice(this);
+        GCMRegistrar.checkManifest(this);
+        final String regId = GCMRegistrar.getRegistrationId(this);
 
-					@Override
-					protected void onPostExecute(Void result) {
-						_registerTask = null;
-					}
+        if (regId == null || regId.equals("")) {
+            Log.d(TAG, "regId is empty");
+            GCMRegistrar.register(getApplicationContext(), SENDER_ID);
+        } else {
+            Log.d(TAG, "registered with regId: " + regId);
+            GCMRegistrar.setRegisteredOnServer(this, false);
+            if (!GCMRegistrar.isRegisteredOnServer(this)) {
+                final Context context = getApplicationContext();
+                _registerTask = new AsyncTask<Void, Void, Void>() {
 
-				};
-				_registerTask.execute(null, null, null);
-			}
-		}
-	}
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        boolean registered = ServerUtilities.register(context,
+                                regId);
+                        /*
+                         * At this point all attempts to register with the app
+                         * server failed, so we need to unregister the device
+                         * from GCM - the app will try to register again when it
+                         * is restarted. Note that GCM will send an unregistered
+                         * callback upon completion, but
+                         * GCMIntentService.onUnregistered() will ignore it.
+                         */
+                        if (!registered) {
+                            GCMRegistrar.unregister(context);
+                        }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+                        return null;
+                    }
 
-	@Override
-	protected void onDestroy() {
-		if (_registerTask != null) {
-			_registerTask.cancel(true);
-		}
-		try {
-			GCMRegistrar.onDestroy(this);
-		} catch (Exception e) {
-			Log.e(TAG, "an error occurred destroying the Activity", e);
-		}
-		
-		super.onDestroy();
-	}
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        _registerTask = null;
+                    }
+
+                };
+                _registerTask.execute(null, null, null);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (_registerTask != null) {
+            _registerTask.cancel(true);
+        }
+        try {
+            GCMRegistrar.onDestroy(this);
+        } catch (Exception e) {
+            Log.e(TAG, "an error occurred destroying the Activity", e);
+        }
+
+        super.onDestroy();
+    }
 
 }
